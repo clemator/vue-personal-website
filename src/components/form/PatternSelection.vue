e2a65ffda5e999b907f1525f7e5296d821273e67
Clément Huon <clemator22@gmail.com>
2019-04-25 22:50:43 +0200

    Add image system

--- src/components/form/PatternSelection.vue
+++ src/components/form/PatternSelection.vue
@@ -1,12 +1,5 @@
 <template>
   <div class="pattern-selection">
-    <svg height="0" width="0">
-      <defs>
-        <clipPath id="svgTextPath">
-          <path fill="#FFFFFF" stroke="#000000" stroke-width="1.5794" d="m 13.22904,270.56726 a 13.204147,13.204261 0 0 0 -13.20416107,13.20428 13.204147,13.204261 0 0 0 9.5708e-4,0.0261 13.204147,13.204261 0 0 1 13.17736399,13.17693 13.204147,13.204261 0 0 0 0.02585,10e-4 13.204147,13.204261 0 0 0 0.02625,-10e-4 13.204147,13.204261 0 0 1 13.176954,-13.17707 13.204147,13.204261 0 0 0 9.59e-4,-0.0258 13.204147,13.204261 0 0 0 -13.204163,-13.20428 z"/>
-        </clipPath>
-      </defs>
-    </svg>
     <div
       class="pattern-selection__container"
     >
@@ -19,7 +12,7 @@
           class="pattern"
           :class="val.NAME"
         >
-          &nbsp;
+          <img :src="require(`@/assets/images/${val.NAME}.svg`)" />
         </div>
       </div>
     </div>
@@ -64,19 +57,7 @@ export default {
         height: 100px;
         width: 100px;
         cursor: pointer;
-        background-color: red;
-        &.diamond {
-          clip-path: $diamond-pattern;
-        }
-        &.hexagon {
-          clip-path: $hexagon-pattern;
-        }
-        &.rubis {
-          clip-path: $rubis-pattern;
-        }
-        &.crescent {
-          clip-path: url(#svgTextPath);
-        }
+        background-color: $light-grey;
       }
     }
   }
